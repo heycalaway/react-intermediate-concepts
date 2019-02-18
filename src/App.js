2e582432dfc5abb41a1d230ab6086a28264c1f09
Jeff Calaway <jeff@heycalaway.com>
2019-02-18 12:10:03 -0600

    Remove Loadable for Redux Practice

--- src/App.js
+++ src/App.js
@@ -1,48 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router } from "@reach/router";
-import Loadable from "react-loadable";
 import { Provider } from "react-redux";
 import store from "./store";
 
-const LoadableNavBar = Loadable({
-  loader: () => import("./NavBar"),
-  loading() {
-    return <h1>loading split out code...</h1>;
-  }
-});
-
-const LoadableDetails = Loadable({
-  loader: () => import("./Details"),
-  loading() {
-    return <h1>loading split out code...</h1>;
-  }
-});
-
-const LoadableResults = Loadable({
-  loader: () => import("./Results"),
-  loading() {
-    return <h1>loading split out code...</h1>;
-  }
-});
-
-const LoadableSearchParams = Loadable({
-  loader: () => import("./SearchParams"),
-  loading() {
-    return <h1>loading split out code...</h1>;
-  }
-});
+import NavBar from "./NavBar";
+import Details from "./Details";
+import Results from "./Results";
+import SearchParams from "./SearchParams";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <LoadableNavBar />
+        <NavBar />
         <Provider store={store}>
           <Router>
-            <LoadableResults path="/" />
-            <LoadableDetails path="/details/:id" />
-            <LoadableSearchParams path="/search-params" />
+            <Results path="/" />
+            <Details path="/details/:id" />
+            <SearchParams path="/search-params" />
           </Router>
         </Provider>
       </div>
